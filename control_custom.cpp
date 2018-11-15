88b251c4ea1ace4aac366c65189f598e313ad25f
Arham Jain <7073362+ajusa@users.noreply.github.com>
2018-11-15 18:11:09 -0500

    time to test the code tomorrow

--- control_custom.cpp
+++ control_custom.cpp
@@ -188,14 +188,23 @@ void Copter::custom_run() {
 enum DIR {Front, Right, Back, Left};
 DIR dir = Front;
 float speed = 50; //5 degrees?
+float crashDistance = 10;
 int i = 0;
+char mode = 'q';
 bool Copter::custom_controller(float &target_climb_rate, float &target_roll, float &target_pitch, float &target_yaw_rate)
 {
     vector<float> dists(4);
-    for (int i = 0; i < 4; ++i) g2.proximity.get_horizontal_distance(i*90, dists[i]);
-    dists.erase(dists.begin() + ((dir + 2) % 4));
     DIR oldDir = dir;
-    dir = (DIR)distance(dists.begin(), max_element(dists.begin(), dists.end())); //this holds the dir with the largest distance
+    for (int i = 0; i < 4; ++i) g2.proximity.get_horizontal_distance(i*90, dists[i]);
+    if(mode == 'q')
+        dists.erase(dists.begin() + ((dir + 2) % 4));
+        dir = (DIR)distance(dists.begin(), max_element(dists.begin(), dists.end())); //largest distance direction
+    else{
+        if(dists[dir] < crashDistance){ //if we hit a wall 10 centimeters away
+            dists.erase(dists.begin() + ((dir + 2) % 4));
+            dir = (DIR)distance(dists.begin(), max_element(dists.begin(), dists.end()));
+        } //then change direction.
+    }
     if(i == 400){ //i is a terrible timer
         //gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Front: %f, Back: %f", distances[0], distances[1]);
         //gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Left: %f, Right: %f", distances[2], distances[3]);
