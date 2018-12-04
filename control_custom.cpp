c0ab25535a92467966079e2a94f0e448f0fc38b4
Arham Jain <7073362+ajusa@users.noreply.github.com>
2018-12-04 16:34:40 -0500

    should handle door

--- control_custom.cpp
+++ control_custom.cpp
@@ -14,6 +14,12 @@ int i = 0;
 int obstacles = 0;
 char mode = 'b';
 int del = 0;
+vector<float> dDoor(10, 0.0); //keeps the last 10 measurements of the door
+int doorPos = 0;
+vector<float> dists(4, 0.0); //create a vector of distances read by the quadcopter
+enum DOOR {None, Out, In};
+DOOR state = None;
+float speed = 0.0;
 // custom_init - initialise custom controller
 bool Copter::custom_init(bool ignore_checks)
 {
@@ -23,6 +29,8 @@ bool Copter::custom_init(bool ignore_checks)
     i = 0;
     obstacles = 0;
     mode = 'b';
+    state = None;
+    speed = 0.0;
     // initialize vertical speeds and leash lengths
     pos_control->set_speed_z(-g.pilot_velocity_z_max, g.pilot_velocity_z_max);
     pos_control->set_accel_z(g.pilot_accel_z);
@@ -121,38 +129,48 @@ bool avoid_door(float &target_climb_rate, float &target_roll, float &target_pitc
     return true;
 }*/
 //this version handles only the door
+
 bool Copter::custom_controller(float &target_climb_rate, float &target_roll, float &target_pitch, float &target_yaw_rate)
 {
-    float speed = g.custom_param2; //get our speed in centi-degrees from custom params
+     //get our speed in centi-degrees from custom params
     float crash = g.custom_param1; //get the distance at which we want to detect an obstacle and turn
     target_pitch = 0;
     target_roll = 0;
-    vector<float> dists(4); //create a vector of distances read by the quadcopter
     for (int i = 0; i < 4; ++i) g2.proximity.get_horizontal_distance(i*90, dists[i]); //fill it with values we read from each direction
-    dists[(dir + 2) % 4] = 0; //make the opposite direction 0
     if(i % 20 == 0){ //used for timing purposes, this code runs 20 times a second instead of 400
-        if(dists[dir] < crash) dir = (DIR)(max_element(dists.begin(), dists.end()) - dists.begin()); //if we see a wall change direction.
-        if(oldDir != dir) obstacles++; //if the direction changed, we saw an obstacle
+        dDoor[doorPos] = dists[0]; //store the last record in the array
+        doorPos = (doorPos + 1) % dDoor.size(); //move to the next slot to be overwritten
+        float total_dDoor = 0;
+        for(int i = 1; i < dDoor.size(); ++i){
+            total_dDoor += dDoor[i] - dDoor[i - 1];
+        } 
+        if(state == None){
+            if(abs(total_dDoor) > g.custom_param3){ //fast door parameter
+                state = total_dDoor > 0 ? In : Out;
+            }
+        } else if(state == Out || state == In){
+            if(abs(total_dDoor) < g.custom_param4){ //close to 0 parameter
+                speed = g.custom_param2; //actually move forward
+            }
+        }
+        if(i == 400){ //i is a terrible timer
+            gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Front: %f, Right: %f", dists[0], dists[1]);
+            gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Back: %f, Left: %f", dists[2], dists[3]);
+            gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Target Pitch: %f, Target Roll: %f", target_pitch, target_roll);
+            char human_door[100];
+            if(state == None) strcpy(human_door, "None");
+            if(state == Out) strcpy(human_door, "Out");
+            if(state == In) strcpy(human_door, "In");
+            gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Door Type %s", human_door);
+            gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "total_dDoor %f", total_dDoor);
+            i = 0;
+        }
     }
-    if(obstacles == 5) return false; //5th obstacle is when we land
     switch(dir){
         case Front: target_pitch = -speed; break;
         case Right: target_roll = speed; break;
         case Back: target_pitch = speed; break;
         case Left: target_roll = -speed; break;
-    }
-    if(i == 400){ //i is a terrible timer
-        gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Front: %f, Right: %f", dists[0], dists[1]);
-        gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Back: %f, Left: %f", dists[2], dists[3]);
-        gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Target Pitch: %f, Target Roll: %f", target_pitch, target_roll);
-        char human_dir[100];
-        if(dir == Front) strcpy(human_dir, "Front");
-        if(dir == Right) strcpy(human_dir, "Right");
-        if(dir == Back) strcpy(human_dir, "Back");
-        if(dir == Left) strcpy(human_dir, "Left");
-        gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Longest Path %s", human_dir);
-        gcs_send_text_fmt(MAV_SEVERITY_CRITICAL, "Obstacles Avoided %d", obstacles);
-        i = 0;
     }
     ++i;
     return true;
